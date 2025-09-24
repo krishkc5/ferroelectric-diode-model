ab7a13c11f0082de0979001d040e9371605e810d
krishna chemudupati <krishkc@seas.upenn.edu>
2025-09-24 15:57:52 -0400

    Add fundamental constants module

---
+++ ferrodiode_model/constants.py
@@ -0,0 +1,23 @@
+"""
+constants.py
+Universal physical constants for ferroelectric diode model
+All values SI units
+"""
+
+# Vacuum permittivity [F/m]
+eps0 = 8.854187e-12
+
+# Elementary charge [C]
+q = 1.602176634e-19
+
+# Planck constant [J * s]
+h = 6.62607015e-34
+
+# Planck constant (reduced) [J * s]
+hbar = 1.054571817e-34
+
+# Electron mass [kg]
+m_e = 9.1093837e-31
+
+# Boltzmann constant [J/K]
+k_B = 1.380649e-23
